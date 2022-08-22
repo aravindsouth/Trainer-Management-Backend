8647b9023d2eb843e909976a10896a5c85a3f351
Aravind A <aravindkerala1@gmail.com>
2022-08-23 02:19:18 +0530

    encrypted password store in db
    - using bcrypt

--- src/routes/signupRoute.js
+++ src/routes/signupRoute.js
@@ -2,63 +2,136 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const userData = require("../model/UserModel");
 const trainerData = require("../model/TrainerModel");
+const bcrypt = require('bcrypt')
 
 const signupRouter = express.Router();
 
-signupRouter.post("/", function (req, res) {
-    var newUser = {
-        name: req.body.fname,
-        dob: req.body.dob,
-        email: req.body.email,
-        mobile: req.body.mobile,
-        highestqual: req.body.hqual,
-        password: req.body.password
-    };
-    console.log('signup process starts');
-    console.log(newUser);
-    userData.findOne({ "username": newUser.email }, (error, user) => {
-        console.log("error=" + error);
-        console.log("user=" + user);
-        if (error) {
-            console.log(error)
+// signupRouter.post("/", function (req, res) {
+//     var newUser = {
+//         name: req.body.fname,
+//         dob: req.body.dob,
+//         email: req.body.email,
+//         mobile: req.body.mobile,
+//         highestqual: req.body.hqual,
+//         password: req.body.password
+//     };
+//     console.log('signup process starts');
+//     console.log(newUser);
+//     userData.findOne({ "username": newUser.email }, (error, user) => {
+//         console.log("error=" + error);
+//         console.log("user=" + user);
+//         if (error) {
+//             console.log(error)
+//         }
+//         else if (user) {
+//             console.log("user exists");
+//             res.json({ status: false, reason: "user exists" });
+//         }
+//         else {
+//             var userAdd = new userData({
+//                 username: newUser.email,
+//                 password: newUser.password,
+//                 role: "trainer"
+//             });
+//             var trainerAdd = new trainerData({
+//                 email: newUser.email,
+//                 dob: newUser.dob,
+//                 name: newUser.name,
+//                 phone: newUser.mobile,
+//                 highestqual: newUser.highestqual,
+//                 approved: false
+//             })
+//             userAdd.save((error, newuser) => {
+//                 console.log('Saved new user=' + newuser)
+//                 console.log("error=" + error)
+//                 if (error) {
+//                     console.log(error)
+//                     res.json({ status: true })
+//                 }
+//                 else {
+//                     trainerAdd.save((error, trainer) => {
+//                         if (error) {
+//                             console.log(error)
+//                         } else {
+//                             console.log("trainer added", trainer)
+//                         }
+//                     })
+//                     res.json({ status: true, reason: "trainer added" }).status(200)
+//                 }
+//             })
+//         }
+//     })
+// })
+
+
+
+// using bcrypt
+signupRouter.post("/", function(req, res){
+    var hashedPassword;
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(error, hash) {
+              
+        if (err) {
+            return console.log('Cannot encrypt');
         }
-        else if (user) {
-            console.log("user exists");
-            res.json({ status: false, reason: "user exists" });
-        }
-        else {
-            var userAdd = new userData({
-                username: newUser.email,
-                password: newUser.password,
-                role: "trainer"
-            });
-            var trainerAdd = new trainerData({
-                email: newUser.email,
-                dob: newUser.dob,
-                name: newUser.name,
-                phone: newUser.mobile,
-                highestqual: newUser.highestqual,
-                approved: false
-            })
-            userAdd.save((error, newuser) => {
-                console.log('Saved new user=' + newuser)
-                console.log("error=" + error)
+            hashedPassword = hash;
+            console.log("hashed password: ", hashedPassword)
+            var newUser = {
+                name: req.body.fname,
+                dob: req.body.dob,
+                email: req.body.email,
+                mobile: req.body.mobile,
+                highestqual: req.body.hqual,
+                password: hashedPassword
+            };
+            console.log("signup started")
+            console.log("user received: ", newUser);
+            userData.findOne({ "username": newUser.email }, (error, user) => {
+                console.log("error=" + error);
+                console.log("user=" + user);
                 if (error) {
                     console.log(error)
-                    res.json({ status: true })
+                }
+                else if (user) {
+                    console.log("user exists");
+                    res.json({ status: false, reason: "user exists" });
                 }
                 else {
-                    trainerAdd.save((error, trainer) => {
+                    var userAdd = new userData({
+                        username: newUser.email,
+                        password: newUser.password,
+                        role: "trainer"
+                    });
+                    var trainerAdd = new trainerData({
+                        email: newUser.email,
+                        dob: newUser.dob,
+                        name: newUser.name,
+                        phone: newUser.mobile,
+                        highestqual: newUser.highestqual,
+                        approved: false
+                    })
+                    userAdd.save((error, newuser) => {
+                        console.log('Saved new user=' + newuser)
+                        console.log("error=" + error)
                         if (error) {
                             console.log(error)
-                        } else {
-                            console.log("trainer added", trainer)
+                            res.json({ status: true })
+                        }
+                        else {
+                            trainerAdd.save((error, trainer) => {
+                                if (error) {
+                                    console.log(error)
+                                } else {
+                                    console.log("trainer added", trainer)
+                                }
+                            })
+                            res.json({ status: true, reason: "trainer added" }).status(200)
                         }
                     })
-                    res.json({ status: true, reason: "trainer added" }).status(200)
                 }
             })
-        }
+
+        })
     })
 })
 
